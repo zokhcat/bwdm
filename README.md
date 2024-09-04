75a8d5022fa44b2e51be0a038f7cbc1c577ac4d2
zokhcat <understandingzak@gmail.com>
2024-09-05 01:39:41 +0530

    feat(packet): Initialize Packet Capture

---
+++ README.md
@@ -0,0 +1,6 @@
+TODO:
+
+- [x] Setting up Packet Capture using `gopacket`
+- [ ] Aggregate the packet sizes to calculate bandwidth and display the data periodically
+- [ ] Use cobra to setup a CLI
+- [ ] Develop the TUI using `tview/termbox-go` to show the chart version
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	interfaceName := "wlo1"
+
+	handle, err := pcap.OpenLive(interfaceName, 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		fmt.Println(packet)
+	}
+}
